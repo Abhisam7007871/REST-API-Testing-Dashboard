06f12d524bdce95af9802d48138f64c00e3a778c
Abhishek <abhisam7007871@gmail.com>
2024-09-27 20:02:49 +0530

    Initial Commit

---
+++ src/main/java/com/avshek/rest_api_test_application/RestApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.avshek.rest_api_test_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avshek/rest_api_test_application/RestApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avshek.rest_api_test_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
